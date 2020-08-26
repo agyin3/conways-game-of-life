b511f26ccb4f5ebd3316d80518b41e20152a55e7
Buddy Agyin <agyin3@gmail.com>
2020-08-26 04:16:13 -0600

    create store and provider

--- index.js
+++ index.js
@@ -3,5 +3,13 @@ import React from "react"
 import ReactDOM from "react-dom"
 import App from './App'
 import './index.css'
+import { createStore } from "redux"
+import { Provider } from "react-redux"
+import { gameReducer } from "./reducers"
 
-ReactDOM.render(<App />, document.getElementById("root"))
+let store = createStore(gameReducer)
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, document.getElementById("root"))
