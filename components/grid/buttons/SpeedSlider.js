8d2d82dc1502ab691c4c8c5b538b7858f01546cc
Buddy Agyin <agyin3@gmail.com>
2020-08-28 09:26:14 -0600

    add button functionality

--- components/grid/buttons/SpeedSlider.js
+++ components/grid/buttons/SpeedSlider.js
@@ -1,12 +1,19 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { setSpeed } from '../../../actions'
 
 const SpeedSlider = () => {
+    const dispatch = useDispatch()
+    const speed = useSelector(state => state.speed)
+    console.log(speed)
+
     return (
         <input
             type='range'
-            min='1'
-            max='10'
-            step='1'
+            min='100'
+            max='500'
+            step='50'
+            onChange={({target}) => dispatch(setSpeed(target.value))}
         />
     )
 }
