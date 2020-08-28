eecea379d927aa6e59633b7487ce0966910cb917
Buddy Agyin <agyin3@gmail.com>
2020-08-28 09:47:02 -0600

    create reset button

--- components/grid/buttons/Buttons.js
+++ components/grid/buttons/Buttons.js
@@ -2,10 +2,12 @@ import React from 'react'
 import PlayButon from './PlayButton'
 import RandomButton from './RandomButton'
 import SpeedSlider from './SpeedSlider'
+import ResetButton from './ResetButton'
 
 const Buttons = () => (
     <div>
         <PlayButon />
+        <ResetButton />
         <RandomButton />
         <SpeedSlider />
     </div>
