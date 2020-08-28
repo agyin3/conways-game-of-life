438f8971b6c0211a2b81281794f8bb145d8aae32
Buddy Agyin <agyin3@gmail.com>
2020-08-28 08:10:29 -0600

    refactor to use global state

--- components/grid/Grid.js
+++ components/grid/Grid.js
@@ -1,155 +1,47 @@
-import React, { useState, useCallback, useRef, Fragment } from "react";
-import produce from "immer";
+import React, { useCallback, useRef, Fragment } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { setRunning } from "../../actions";
-
-const numRows = 20;
-const numCols = 20;
+import { setRunning, runGeneration, clickCell } from "../../actions";
 
 const Grid = () => {
-  const runningRef = useRef();
-  const dispatch= useDispatch()
-  const [running] = useSelector(state => state.running)
+  const runningRef = useRef()
+  const gridRef = useRef()
+  const dispatch = useDispatch()
+  const {running, grid, numRows, numCols, speed} = useSelector(state => state)
+
   runningRef.current = running;
-  const [grid, setGrid] = useState(() => {
-    let newGrid = [];
-    for (let i = 0; i < numRows; i++) {
-      newGrid.push(Array.from(Array(numCols), () => 0));
-    }
-    return newGrid;
-  });
+  gridRef.current = grid
 
   // useCallback will only create the function once
   const runSimulation = useCallback(() => {
     if (!runningRef.current) {
       return;
     }
+    dispatch(runGeneration(gridRef.current, numRows, numCols))
 
-    setGrid((g) => {
-      return produce(g, (gridCopy) => {
-        for (let i = 0; i < numRows; i++) {
-          for (let j = 0; j < numCols; j++) {
-            // Step 1 - Finding Neighbors
-
-            let neighbors = 0;
-
-            // All cells not on border
-            if (i !== 0 && i !== (numRows - 1) && j !== 0 && j !== (numCols - 1)) {
-              neighbors =
-                g[i - 1][j - 1] +
-                g[i - 1][j] +
-                g[i - 1][j + 1] +
-                g[i][j - 1] +
-                g[i][j + 1] +
-                g[i + 1][j - 1] +
-                g[i + 1][j] +
-                g[i + 1][j + 1];
-            }
-
-            // Grid Top Row
-            else if (i === 0) {
-              // Top Left Corner
-              if (j === 0) {
-                neighbors = g[i][j + 1] + g[i + 1][j + 1] + g[i + 1][j];
-              }
-              // Top Right Corner
-              else if (j === numCols - 1) {
-                neighbors = g[i][j - 1] + g[i + 1][j - 1] + g[i + 1][j];
-              }
-
-              // All other cases
-              else {
-                neighbors =
-                  g[i][j + 1] +
-                  g[i + 1][j + 1] +
-                  g[i + 1][j] +
-                  g[i + 1][j - 1] +
-                  g[i][j - 1];
-              }
-            }
-
-            // Grid Left Row
-            else if (j === 0) {
-              // Bottom Left Corner
-              if (i === numRows - 1) {
-                neighbors = g[i - 1][j] + g[i - 1][j + 1] + g[i][j + 1];
-              }
-
-              // All other cases
-              else {
-                neighbors =
-                  g[i - 1][j] +
-                  g[i - 1][j + 1] +
-                  g[i][j + 1] +
-                  g[i + 1][j] +
-                  g[i + 1][j + 1];
-              }
-            }
-
-            // Grid Right Row
-            else if (j === numCols - 1) {
-              // Bottom Right Corner
-              if (i === numRows - 1) {
-                neighbors = g[i - 1][j] + g[i - 1][j - 1] + g[i][j - 1];
-              }
-
-              // All other Cases
-              else {
-                neighbors =
-                  g[i - 1][j] +
-                  g[i - 1][j - 1] +
-                  g[i][j - 1] +
-                  g[i + 1][j - 1] +
-                  g[i + 0][j];
-              }
-            }
-
-            // Grid Bottom Row
-            else {
-              neighbors =
-                g[i - 1][j] +
-                g[i - 1][j - 1] +
-                g[i][j - 1] +
-                g[i - 1][j + 1] +
-                g[i][j];
-            }
-
-            // Step 2 - Updating Cells
-
-            // Cell is alive(1)
-            if (neighbors < 2 || neighbors > 3) {
-                gridCopy[i][j] = 0
-            }else if(g[i][j] === 0 && neighbors === 3){
-                gridCopy[i][j] = 1
-            }
-          }
-        }
-      });
-    });
-
-    setTimeout(runSimulation, 1000);
+    setTimeout(runSimulation, speed);
   }, []);
 
+
   const handleCellClick = (i, j) => {
-    const newGrid = produce(grid, (gridCopy) => {
-      gridCopy[i][j] = gridCopy[i][j] ? 0 : 1;
-    });
-    setGrid(newGrid);
+    dispatch(clickCell(i, j));
   };
+
+  const handlePlayClick = (running) => {
+    dispatch(setRunning(!running));
+      if (!running) {
+        runningRef.current = true;
+        runSimulation();
+      }
+  }
 
   return (
     <Fragment>
       <button
-        onClick={() => {
-          dispatch(setRunning(!running));
-          if (!running) {
-            runningRef.current = true;
-            runSimulation();
-          }
-        }}
+        onClick={() => handlePlayClick(running)}
       >
-        Start
+        {running ? 'Stop' : 'Start'}
       </button>
+      <h2></h2>
       <div
         style={{
           display: "grid",
