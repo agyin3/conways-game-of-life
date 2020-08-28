e74e0549d14caba4c5b7ab448128364b7699a69a
Buddy Agyin <agyin3@gmail.com>
2020-08-28 08:12:05 -0600

    create buttons

---
+++ components/grid/buttons/Buttons.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PlayButon from './PlayButton'
+import RandomButton from './RandomButton'
+import SpeedSlider from './SpeedSlider'
+
+const Buttons = () => (
+    <div>
+        <PlayButon />
+        <RandomButton />
+        <SpeedSlider />
+    </div>
+)
+
+export default Buttons
---
+++ components/grid/buttons/RandomButton.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const RandomButton = () => {
+    return (
+        <button 
+        type='button'
+        onClick={() => console.log('hello')}
+        >
+            Random
+        </button>
+    )
+}
+
+export default RandomButton
